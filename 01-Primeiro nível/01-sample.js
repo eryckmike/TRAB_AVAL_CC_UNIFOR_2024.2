3a2f6585421919b2ee228ac0df9bc514f5d1c296
Eryck <eryckmike@gmail.com>
2024-11-11 11:16:36 -0300

    atualizado

--- 01-Primeiro nível/01-sample.js
+++ 01-Primeiro nível/01-sample.js
@@ -1,5 +1,5 @@
 function h(a) {
-    let b = [];
+    const b = [];
     for (let i = 0; i < a.length; i++) {
         if (a[i] % 2 === 0) {
             b.push(a[i]);
@@ -7,7 +7,7 @@ function h(a) {
     }
     return b;
 }
-
-let x = [1, 2, 3, 4, 5, 6];
-let y = h(x);
+const numeros = [1, 2, 3, 4, 5, 6]
+const x = [1, 2, 3, 4, 5, 6];
+const y = h(x);
 console.log(y);
