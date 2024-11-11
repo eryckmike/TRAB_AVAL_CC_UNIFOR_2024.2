add5f2314efcc7aa4fbbe8b98cb4383e1c7e206d
Eryck <eryckmike@gmail.com>
2024-11-11 11:15:36 -0300

    atualizado

--- 02-Segundo nível/05-sample.js
+++ 02-Segundo nível/05-sample.js
@@ -1,28 +1,35 @@
 function calcularSalarioFuncionario(horasTrabalhadas, valorHora, cargo) {
     const salarioBase = horasTrabalhadas * valorHora;
-
+    const bonusGerente = 1000;
+    const bonusSupervisor = 500;
+    const bonusFuncionario = 200;
     let salarioComBonus;
-    if (cargo === 'gerente') {
-        salarioComBonus = salarioBase + 1000;
-    } else if (cargo === 'supervisor') {
-        salarioComBonus = salarioBase + 500;
+    if (cargo === "gerente") {
+        salarioComBonus = salarioBase + bonusGerente;
+    } else if (cargo === "supervisor") {
+        salarioComBonus = salarioBase + bonusSupervisor;
     } else {
-        salarioComBonus = salarioBase + 200;
+        salarioComBonus = salarioBase + bonusFuncionario;
     }
-
-    const salarioComDesconto = salarioComBonus - 300;
-
+    const desconto = 300;
+    const salarioComDesconto = salarioComBonus - desconto;
+    const porcentagemAlta = 0.27;
+    const porcentagemMedia = 0.18;
+    const porcentagembaixa = 0.11;
+    const padraoSalarioAlto = 5000;
+    const padraoSalarioMedio = 3000;
     let salarioFinal;
-    if (salarioComDesconto > 5000) {
-        salarioFinal = salarioComDesconto - (salarioComDesconto * 0.27);
-    } else if (salarioComDesconto > 3000) {
-        salarioFinal = salarioComDesconto - (salarioComDesconto * 0.18);
+    if (salarioComDesconto >padraoSalarioAlto) {
+        salarioFinal = salarioComDesconto - (salarioComDesconto * porcentagemAlta);
+    } else if (salarioComDesconto > padraoSalarioMedio) {
+        salarioFinal = salarioComDesconto - (salarioComDesconto * porcentagemMedia);
     } else {
-        salarioFinal = salarioComDesconto - (salarioComDesconto * 0.11);
+        salarioFinal = salarioComDesconto - (salarioComDesconto * porcentagembaixa);
     }
-
     return salarioFinal;
 }
-
-const salario = calcularSalarioFuncionario(160, 25, 'gerente');
-console.log(`O salário final é: ${salario}`);
+const horasTeste = 160;
+const valorHoraTeste = 25;
+const salario = calcularSalarioFuncionario(horasTeste, valorHoraTeste, "gerente");
+const quantidadeNumeroPosVirgula = 2;
+console.log(`O salário final é: ${salario.toFixed(quantidadeNumeroPosVirgula)}`);
